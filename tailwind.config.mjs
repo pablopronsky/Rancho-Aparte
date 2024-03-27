8d961ec61ba26b796a2dafeab815e22bb4575445
pablopronsky <pronskypablo@gmail.com>
2024-03-26 22:57:24 -0300

    almost finished

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,15 @@ export default {
 		  'gray-medium': '#A9A9A9',
 		  'gray-dark': '#404040',
 		},
+		keyframes: {
+			wiggle: {
+			  '0%, 100%': { transform: 'rotate(-3deg)' },
+			  '50%': { transform: 'rotate(3deg)' },
+			},
+		  },
+		  animation: {
+			wiggle: 'wiggle 1s ease-in-out infinite',
+		  },
 	  },
 	},
 	plugins: [],
